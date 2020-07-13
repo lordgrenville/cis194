83472025958315985a6962c5a2a3d1dbae778f04
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2020-07-13 15:24:19 +0300

    week 3 done

--- wk3/Golf.hs
+++ wk3/Golf.hs
@@ -1,4 +1,5 @@
 module Golf where
+import Data.List
 
 skips :: [a] -> [[a]]
 skips xs = map h [1..length xs] where h = \y -> map fst $ filter (\x -> snd x `mod` y == 0) $ zip xs [1..length xs]
@@ -11,4 +12,22 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima (a:xs@(b:c:_)) = [b | b > a && b > c] ++ localMaxima xs
 localMaxima _ = []
 
+getFreqTuples :: [Integer] -> [(Integer, Integer)]
+getFreqTuples = map (\x -> (head x, toInteger $ length x)) . group . sort
+
+getMaxRepeat :: [Integer] -> Integer
+getMaxRepeat x = toInteger $ maximum $ map snd $ getFreqTuples x
+
+genRow :: [Bool] -> String
+genRow x = unwords $ map (\x -> if x then "*" else " ") x
+
+rowFromList :: [(Integer, Integer)] -> Integer -> [Bool]
+--given a set of tuples and row index, return those from orig list with at
+--least index-many apearancwes
+rowFromList xs n =  map (`elem` li) [0..9] where li = nub $ map fst $ filter (\x -> snd x >= n) xs
+
+getBoolArrays :: [Integer] -> [[Bool]]
+getBoolArrays x = map (rowFromList y) $ (\z -> [1..getMaxRepeat z]) x where y = getFreqTuples x
+
 histogram :: [Integer] -> String
+histogram x = unlines (reverse (map genRow $ getBoolArrays x)) ++ "===================\n0 1 2 3 4 5 6 7 8 9\n"
